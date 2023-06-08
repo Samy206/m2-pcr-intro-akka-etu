9e829a968b09739f366c86e24ce6b017766eb6d6
Samy206 <samy.boumali@hotmail.com>
2023-06-08 08:01:06 +0200

    Erathosthene fini

---
+++ src/main/java/m2dl/pcr/akka/erathostene/FilterActor.java
@@ -0,0 +1,54 @@
+package m2dl.pcr.akka.erathostene;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.japi.Procedure;
+import m2dl.pcr.akka.helloworld1.SimpleHelloWorldActor;
+import scala.Int;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FilterActor extends UntypedActor {
+
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+    private int filterValue;
+    List<ActorRef> filters;
+
+    public FilterActor(int value) {
+        log.info("New Cribble created");
+        filterValue = value;
+        filters = new ArrayList<ActorRef>();
+    }
+
+    Procedure<Object> filter = new Procedure<Object>() {
+        public void apply(Object msg) throws Exception {
+            if(msg instanceof Integer) {
+                Integer givenValue = (Integer) msg;
+                if(givenValue % filterValue != 0 && filters.size() == 0) {
+                    ActorRef ref = getContext().actorOf(Props.create(FilterActor.class, givenValue), "filter-actor-"+ givenValue);
+                    filters.add(ref);
+                    log.info("New prime number has been added : " + givenValue);
+                }
+                else if(givenValue % filterValue != 0 && filters.size() > 0) {
+                    for(ActorRef ref : filters) {
+                        ref.tell(givenValue, null);
+                    }
+                }
+            } else {
+                unhandled(msg);
+            }
+        }
+    };
+
+
+    @Override
+    public void onReceive(Object o) throws Exception {
+        filter.apply(o);
+    }
+}
---
+++ src/main/java/m2dl/pcr/akka/erathostene/System.java
@@ -0,0 +1,35 @@
+package m2dl.pcr.akka.erathostene;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import m2dl.pcr.akka.helloworld3.HelloGoodbyeActor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class System {
+
+    public static final Logger log = LoggerFactory.getLogger(m2dl.pcr.akka.helloworld3.System.class);
+
+    public static void main(String [] args) throws InterruptedException {
+        final ActorSystem actorSystem = ActorSystem.create("actor-system");
+
+        Thread.sleep(5000);
+
+        final ActorRef actorRef = actorSystem.actorOf(Props.create(FilterActor.class, 2), "filter-actor");
+
+        for(int i = 2; i < 100; i++) {
+            actorRef.tell(i, null);
+        }
+
+
+        Thread.sleep(1000);
+
+        log.debug("Actor System Shutdown Starting...");
+
+        actorSystem.terminate();
+    }
+
+}
